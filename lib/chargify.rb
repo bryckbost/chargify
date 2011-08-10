08ace85d688f33da6af93ed8e8976b7aff482d73
Steve Richert <steve.richert@gmail.com>
2011-08-09 20:45:57 -0400

    Clean up Chargify base namespace

--- lib/chargify.rb
+++ lib/chargify.rb
@@ -4,16 +4,15 @@ require "chargify/configuration"
 module Chargify
   extend Configuration
 
-  def self.new(options = {})
-    Chargify::Client.new(options)
+  def self.client
+    @client ||= Client.new
   end
 
   def self.method_missing(method, *args, &block)
-    instance = new
-    instance.respond_to?(method) ? instance.send(method, *args, &block) : super
+    client.respond_to?(method) ? client.send(method, *args, &block) : super
   end
 
-  def respond_to?(method, include_private = false)
-    new.respond_to?(method, include_private) || super(method, include_private)
+  def self.respond_to?(*args)
+    client.respond_to?(*args) || super
   end
 end
