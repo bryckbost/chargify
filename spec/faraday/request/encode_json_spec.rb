166e49b01734c119541a17276599d9d70b5692bd
Steve Richert <steve.richert@gmail.com>
2011-08-24 06:16:01 -0400

    Only encode present request bodies

--- spec/faraday/request/encode_json_spec.rb
+++ spec/faraday/request/encode_json_spec.rb
@@ -9,6 +9,12 @@ describe Faraday::Request::EncodeJson do
     env.should == {}
   end
 
+  it "ignores nil bodies" do
+    env = {:body => nil}
+    middleware.call(env)
+    env.should == {:body => nil}
+  end
+
   it "encodes array bodies" do
     env = {:body => ["foo", "bar"]}
     middleware.call(env)
@@ -19,43 +25,5 @@ describe Faraday::Request::EncodeJson do
     env = {:body => {:foo => "bar"}}
     middleware.call(env)
     env.should == {:body => %({"foo":"bar"})}
-  end
-
-  context "unnecessarily" do
-    it "encodes string bodies" do
-      env = {:body => "foo"}
-      middleware.call(env)
-      env.should == {:body => %("foo")}
-    end
-
-    it "encodes integers" do
-      env = {:body => 1}
-      middleware.call(env)
-      env.should == {:body => %(1)}
-    end
-
-    it "encodes floats" do
-      env = {:body => 1.0}
-      middleware.call(env)
-      env.should == {:body => %(1.0)}
-    end
-
-    it "encodes true" do
-      env = {:body => true}
-      middleware.call(env)
-      env.should == {:body => %(true)}
-    end
-
-    it "encodes false" do
-      env = {:body => false}
-      middleware.call(env)
-      env.should == {:body => %(false)}
-    end
-
-    it "encodes nil" do
-      env = {:body => nil}
-      middleware.call(env)
-      env.should == {:body => %(null)}
-    end
   end
 end
